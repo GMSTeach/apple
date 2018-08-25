7ad147df13153c33a2ddcc7b4585d3fff48cb15a
민대홍 <mdh0907@naver.com>
2018-08-25 15:18:47 +0900

    firstcommit

---
+++ app/src/main/java/com/example/user/gmsteach_1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.gmsteach_1;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.i);
+        textView.setText("안녕~");
+        textView.setTextColor(Color.BLUE);
+    }
+}
